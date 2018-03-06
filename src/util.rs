1228354e1fcd833298e965e316a3a1c53ec80bcc
Simon Heath <icefoxen@gmail.com>
2018-03-06 12:20:23 -0500

    Added utility functions to detect signaling NaN's
    Even if they're not used anywhere yet.

--- src/util.rs
+++ src/util.rs
@@ -65,3 +65,88 @@ pub fn u64_to_f64(i: u64) -> f64 {
     // disallowed by wasm.  :-P
     f64::from_bits(i)
 }
+
+/// Returns whether or not the float is a signaling NaN.
+/// A signaling NaN has a format like:
+/// `s111 1111 1nxx xxxx xxxx xxxx xxxx xxxx`
+/// where the `x`'s represent a non-zero number (zero
+/// would be infinity) and `n` is 0.
+/// The sign bit `s` may be anything.
+///
+/// On some old-fashioned platforms (PA-RISC, some MIPS)
+/// a signaling NaN is marked by `n=1`, but oh well.
+pub fn f32_is_signaling_nan(f: f32) -> bool {
+    let uf: u32 = f.to_bits();
+    let signal_bit = 0b0000_0000_0100_0000_0000_0000_0000_0000;
+    let signal_bit = 1 << 22;
+    // signaling nan := is NAN and signal bit is clear
+    let signal_bit_clear = (uf & signal_bit) == 0;
+    f32::is_nan(f) && signal_bit_clear
+}
+
+/// Same as `f32_is_signaling_nan()` for `f64`'s.
+/// The signaling-nan-bit is bit 51 instead of bit 22
+pub fn f64_is_signaling_nan(f: f64) -> bool {
+    let uf: u64 = f.to_bits();
+    let signal_bit = 1 << 51;
+    // signaling nan := is NAN and signal bit is clear
+    let signal_bit_clear = (uf & signal_bit) == 0;
+    f64::is_nan(f) && signal_bit_clear
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_f32_is_signaling_nan() {
+        use std::f32;
+        assert!(!f32_is_signaling_nan(3.0));
+        assert!(!f32_is_signaling_nan(f32::NAN));
+        assert!(!f32_is_signaling_nan(f32::INFINITY));
+        assert!(!f32_is_signaling_nan(f32::NEG_INFINITY));
+        assert!(!f32_is_signaling_nan(f32::MAX));
+        assert!(!f32_is_signaling_nan(f32::MIN));
+
+        // Create a signaling NaN by taking f32::NAN and setting
+        // bit 22 to 0
+        // Then in case that was the only bit set in the mantissa,
+        // set bit 0 too
+        let mask: u32 = !(1 << 22);
+        let uf = f32::NAN.to_bits();
+        let signaling_nan = f32::from_bits(uf & mask | 1);
+        //println!();
+        //println!("{:b}", uf);
+        //println!("{:b}", mask);
+        //println!("{:b}", uf & mask);
+        assert!(f32_is_signaling_nan(signaling_nan));
+    }
+
+    #[test]
+    fn test_f64_is_signaling_nan() {
+        use std::f64;
+        assert!(!f64_is_signaling_nan(3.0));
+        assert!(!f64_is_signaling_nan(f64::NAN));
+        assert!(!f64_is_signaling_nan(f64::INFINITY));
+        assert!(!f64_is_signaling_nan(f64::NEG_INFINITY));
+        assert!(!f64_is_signaling_nan(f64::MAX));
+        assert!(!f64_is_signaling_nan(f64::MIN));
+
+        // Create a signaling NaN by taking f64::NAN and setting
+        // bit 51 to 0
+        // Then in case that was the only bit set in the mantissa,
+        // set bit 0 too
+        let mask: u64 = !(1 << 51);
+        let uf = f64::NAN.to_bits();
+        let signaling_nan = f64::from_bits(uf & mask | 1);
+        //println!();
+        //println!("{:b}", uf);
+        //println!("{:b}", mask);
+        //println!("{:b}", uf & mask | 1);
+        assert!(f64_is_signaling_nan(signaling_nan));
+    }
+
+}
